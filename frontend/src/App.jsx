50e24e1b96810c10ac2b9b4ed408aedfeecf6347
codesforjenuel <codesforjenuel@gmail.com>
2023-10-14 02:41:25 -0700

    create routes to all pages that will be available in the webapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+//importing Routes and Route, will allow us to configure the routes in our project
+import {Routes, Route} from 'react-router-dom'
+//folowing imports the pages so we can use them as routes
+import Blog from './pages/blog'
+import Drivers from './pages/Drivers'
+import Shop from './pages/shop'
+import Trending from './pages/trending'
+import Videos from './pages/videos'
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-cyan-400 font-bold underline">
-    Hello world!
-  </h1>
+   <Routes>
+<Route path='/' element={<Blog />}/>
+<Route path='/drivers' element={<Drivers />}/>
+<Route path='/shop' element={<Shop />}/>
+<Route path='/trending' element={<Trending />}/>
+<Route path='/videos' element={<Videos />}/>
+   </Routes>
   )
 }
 
