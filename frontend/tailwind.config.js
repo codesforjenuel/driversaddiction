9a3ded70f7a770f12a34bb2d0dc4c8c220bdc7b8
codesforjenuel <codesforjenuel@gmail.com>
2023-10-15 10:56:41 -0700

    nav is fully responsive to all kinds of devices, from mobile to tables and laptop screens

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '416px',
+      // => @media (min-width: 400px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    fontSize: {
+      sm: '0.5rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     extend: {},
   },
   plugins: [],
