56876f0751d7c6ca77478514edba9df31d527d7c
Revivedaniel <mlg.danielstark@gmail.com>
2023-10-17 11:44:25 -0700

    Updated the port variable to accept PORT from env and added logic to send built react app to frontend on every request.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
