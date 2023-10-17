395e3115ee8f262c8806f0746a776cc32202bbe0
codesforjenuel <codesforjenuel@gmail.com>
2023-10-17 16:10:47 -0700

    videos page is completed with responsiveness available for mobile, tables, laptops and large screens

--- frontend/src/components/Dotw.jsx
+++ frontend/src/components/Dotw.jsx
@@ -8,8 +8,8 @@ const Dotw = () => {
 <div className=' w-3/4 flex flex-col justify-around md:flex-row items-center border-solid border-y-2 border-darkgray'>
 <div className="shadow-lg w-56 h-56 m-8 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-md bg-no-repeat bg-cover bg-center bg-[url('https://imageio.forbes.com/specials-images/imageserve/63d5c9ccd110211cca4c5504/Doug-DeMuro-Ford-GT/960x0.jpg?format=jpg&width=960')]"></div>
 <div className='flex flex-col m-8 h-full'>
-<h2 className='text-center font-normal text-2xl '>Driver's Name</h2>
-<p className='  md:text-xl  font-sans md:w-80 lg:w-96 md:leading-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum natus tenetur sunt earum tempore perferendis accusantium placeat mollitia. Obcaecati, vero nihil eaque corporis exercitationem dolore illum tempora culpa ipsum accusantium?</p>
+<h2 className='text-center font-normal text-2xl '>DOUG DEMURO</h2>
+<p className='  md:text-xl  md:w-80 lg:w-96 md:leading-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum natus tenetur sunt earum tempore perferendis accusantium placeat mollitia. Obcaecati, vero nihil eaque corporis exercitationem dolore illum tempora culpa ipsum accusantium?</p>
  </div>  
    </div> 
     </div>
--- frontend/src/pages/Videos.jsx
+++ frontend/src/pages/Videos.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
 const Videos = () => {
   return (
-    <div>
+    <div className='m-0 h-full w-full flex flex-col items-center bg-white'>
       <Nav/>
-      <h1>Videos</h1>
+      {/* this div contains the banner */}
+      <div className="w-full h-40 md:h-72 lg:h-96 max-w-screen-lg  bg-no-repeat bg-cover bg-center lg:bg-center bg-[url('https://res.cloudinary.com/dkaeetuud/image/upload/v1697577890/Drivers%20Addiction/collage_xcwmey.png')]">
+    </div>
+    {/* this div is the container for the lastest video */}
+    <div className=' w-full max-w-screen-lg h-full flex flex-col items-center shadow-md'>
+      <h2 className='text-xl font-light md:text-5xl drop-shadow-[0_7px_4px_rgba(0,0,0,0.25)] text-left m-10'>LATEST VIDEO</h2>
+      <div className='w-full max-w-screen-lg h-full flex flex-col items-center border-y-2 border-solid border-slate-300'>
+      <h3 className='text-base font-light md:text-3xl drop-shadow-[0_7px_4px_rgba(0,0,0,0.25)] text-left m-5'>2024 Nissan Z Nismo Edition</h3>
+      <iframe className='w-full sm:h-96 md:h-500 md:w-3/4 h-60 mb-10' src="https://www.youtube.com/embed/ETzVuiulfXM?si=j8mJvduvp3GulDwI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    </div>
+    <div className=' w-full max-w-screen-lg h-full flex flex-col items-center shadow-md'>
+      <h2 className='text-xl font-light md:text-5xl drop-shadow-[0_7px_4px_rgba(0,0,0,0.25)] text-left m-10'>SHORTS</h2>
+      <div className='w-full max-w-screen-lg h-full flex flex-col md:flex-wrap md:flex-row items-center justify-center border-y-2 border-solid border-slate-300'>
+      <iframe className='w-56 sm:h-96 md:h-500 md:w-72 h-96 m-5' src="https://youtube.com/embed/3dccPeN2R3E?si=70dx6ajvchIlz1Bo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className='w-56 sm:h-96 md:h-500 md:w-72 h-96 m-5' src="https://youtube.com/embed/9NuH3Nxguec?si=jvWe7ahj2n8K5eyc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className='w-56 sm:h-96 md:h-500 md:w-72 h-96 m-5' src="https://youtube.com/embed/3vOuIchrdVE?si=EAykfk59evCiLpQi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    </div>
+    <Footer/>
     </div>
   )
 }
