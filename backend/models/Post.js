148b345fd28ee72d5f43141d9d8dadb968729c24
Revivedaniel <mlg.danielstark@gmail.com>
2023-10-17 12:35:21 -0700

    Started to create post model

---
+++ backend/models/Post.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+// TODO: Define Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
