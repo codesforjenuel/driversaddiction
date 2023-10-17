95a7d39c31d3d46561f55ea186290e347ec69075
Revivedaniel <mlg.danielstark@gmail.com>
2023-10-17 12:31:10 -0700

    Added SQL DB connection, ORM, and session storage

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const path = require('path');
 const routes = require('./controllers');
+const session = require('express-session');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// TODO: Adjust cookie settings
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
@@ -15,6 +36,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
