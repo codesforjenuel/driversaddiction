ece74ebb29831a08bfa72673bb3a3892a00c9dea
Revivedaniel <mlg.danielstark@gmail.com>
2023-10-21 21:40:46 -0700

    Created Trending Post model

---
+++ backend/models/TrendingPost.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
+
+// TrendingPost Model
+class TrendingPost extends Model {}
+TrendingPost.init({
+  heroImage: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  story: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  videoId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Videos', // 'Videos' is the table name for the Video model
+      key: 'id'
+    }
+  }
+}, {
+  sequelize,
+  modelName: 'TrendingPost'
+})
+
+module.exports = TrendingPost
