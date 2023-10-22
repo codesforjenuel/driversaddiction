46488495343514edd6f1a3e0170d5d431c9c155b
Revivedaniel <mlg.danielstark@gmail.com>
2023-10-21 21:41:28 -0700

    Created table associations

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,16 @@
 const User = require('./User')
 const Post = require('./Post')
+const DriverOfTheWeek = require('./driverOfTheWeek')
+const DriverSocial = require('./DriverSocial')
+const Video = require('./Video')
+const TrendingPost = require('./TrendingPost')
 
 // TODO: Create table associations
+DriverOfTheWeek.hasMany(DriverSocial, { as: 'socials', foreignKey: 'driverProfileId' })
+DriverSocial.belongsTo(DriverOfTheWeek, { foreignKey: 'driverProfileId' })
+DriverOfTheWeek.belongsTo(Video, { foreignKey: 'videoId', as: 'video' })
+Video.hasOne(DriverOfTheWeek, { foreignKey: 'videoId' })
+TrendingPost.belongsTo(Video, { foreignKey: 'videoId', as: 'video' })
+Video.hasOne(TrendingPost, { foreignKey: 'videoId' })
 
-module.exports = { User, Post }
+module.exports = { DriverOfTheWeek, DriverSocial, Video, TrendingPost, User, Post }
