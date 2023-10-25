335a5b87bcf325be430995571f1bd72e7ec6eee3
codesforjenuel <codesforjenuel@gmail.com>
2023-10-24 22:05:48 -0700

    forms to update manage page are done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,9 +8,9 @@ const Login = () => {
       <Nav/>
       <div className='h-500 md:h-700 lg:h-1000 w-full bg-gradient-to-br from-zinc-500 to-white flex items-center justify-center'>
 <div className='w-60 h-96 bg-white drop-shadow-lg flex flex-col items-center justify-center'>
-  <label for="email" className='mt-5'>EMAIL</label>
+  <label htmlFor="email" className='mt-5'>EMAIL</label>
   <input type="email" name="email" className=' border-2 border-solid border-slate-500 rounded-md mb-5'required />
-  <label for="password">PASSWORD</label>
+  <label htmlFor="password">PASSWORD</label>
   <input type="password" name="password" className=' border-2 border-solid border-slate-500 rounded-md mb-5' required />
 <div>
   <button className='w-20 bg-slate-300 rounded-md p-2 hover:bg-slate-400 transition-transform m-2'>LOGIN</button>
