38535ba5318d5d08fd907e0301357963560eea95
codesforjenuel <codesforjenuel@gmail.com>
2023-11-20 21:37:40 -0800

    updated blogform to use jwt authentication

--- frontend/src/components/Blogform.jsx
+++ frontend/src/components/Blogform.jsx
@@ -1,11 +1,12 @@
 import React, {useState,useEffect} from 'react'
+import auth from '../utils/auth'
 
 const Blogform = () => {
-  const [heroImage, setHeroImage] = useState(null)
-  const [profileImage, setProfileImage] = useState(null)
-  const [driversName, setDriversName] = useState(null)
-  const [driverBio, setDriverBio] = useState(null)
-  const [story, setStoryInfo] = useState(null)
+  const [heroImage, setHeroImage] = useState("")
+  const [profileImage, setProfileImage] = useState("")
+  const [driversName, setDriversName] = useState("")
+  const [driverBio, setDriverBio] = useState("")
+  const [story, setStoryInfo] = useState("")
   const URL = 'http://localhost:3001/api/driverOfTheWeek'
 
   useEffect(() => {
@@ -26,17 +27,18 @@ try {
 
 
 const dataToSubmit = {
-  heroImage:heroImage,
-  profileImage:profileImage,
-  driversName:driversName,
-  driverBio:driverBio,
-  story:story,
+  heroImage,
+  profileImage,
+  driversName,
+  driverBio,
+  story,
 }
-fetch('http://localhost:3001/api/driverOfTheWeek/1', {
+fetch('http://localhost:3001/api/driverOfTheWeek/655c31dcd615300eba94d16f', {
   method: 'PUT' ,
   headers: {
-    'Content-type' : 'application/json; charset=UTF-8'
-  },
+    'Content-type' : 'application/json; charset=UTF-8',
+    'authorization' : `Bearer ${auth.getToken()}`
+  }, 
   body: JSON.stringify(dataToSubmit)
 }).then(res => res.json())
 .then(res => {
@@ -51,15 +53,15 @@ fetch('http://localhost:3001/api/driverOfTheWeek/1', {
     
       <form action="#" method="put" className='w-60 h-full bg-white drop-shadow-lg flex flex-col items-center' onSubmit={handleFormSubmit}>
     <label htmlFor="hero" className='mt-5'>Hero Image URL</label>
- <input type="text" name="hero" value={heroImage} className=' border-2 border-solid border-slate-500 rounded-md mb-5'required onSubmit={(event) => setHeroImage(event.target.value)} />
+ <input type="text" name="hero" value={heroImage} className=' border-2 border-solid border-slate-500 rounded-md mb-5'required onChange={(event) => setHeroImage(event.target.value)} />
  <label htmlFor="profile">Profile Image URL</label>
- <input type="text" name="profile" value={profileImage} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onSubmit={(event) => setProfileImage(event.target.value)}/>
+ <input type="text" name="profile" value={profileImage} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onChange={(event) => setProfileImage(event.target.value)}/>
  <label htmlFor="name">Driver's Name</label>
- <input type="text" name="name" value={driversName} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onSubmit={(event) => setDriversName(event.target.value)}/>
+ <input type="text" name="name" value={driversName} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onChange={(event) => setDriversName(event.target.value)}/>
  <label htmlFor="bio">Bio</label>
- <input type="text" name="bio" value={driverBio} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onSubmit={(event) => setDriverBio(event.target.value)}/>
+ <input type="text" name="bio" value={driverBio} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onChange={(event) => setDriverBio(event.target.value)}/>
  <label htmlFor="story">Story</label>
- <input type="text" name="story" value={story} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onSubmit={(event) => setStoryInfo(event.target.value)}/>
+ <input type="text" name="story" value={story} className=' border-2 border-solid border-slate-500 rounded-md mb-5' required onChange={(event) => setStoryInfo(event.target.value)}/>
 
 
 
