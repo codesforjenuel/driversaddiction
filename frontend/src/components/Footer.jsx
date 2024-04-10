4d477735cd9bb4f58d50b1d29512d0eeede41841
Jesus Perea <g-sus-15@hotmail.com>
2024-04-10 01:19:32 -0700

    adjusted useState defined values

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,9 +6,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className='w-full p-10 text-center flex flex-col items-center bg-white'>
-        <h3>© 2023 Drivers Addiction. All rights reserved.
-Any unauthorized reproduction or distribution of the content on this website is prohibited.
-</h3>
+        <h3>© 2023 Drivers Addiction. All rights reserved.</h3>
 <div className=' w-full flex flex-row justify-center'>
         <Link to="https://www.instagram.com/drivers.addiction/" target='https://www.instagram.com/drivers.addiction/'><FaInstagram size={30} color="#bc2a8d" className='m-5 drop-shadow-[0_2px_1px_rgba(0,0,0,0.25)] hover:drop-shadow-[0_4px_1px_rgba(0,0,0,0.25)] transition duration-5s' /></Link>
         <Link to="https://www.youtube.com/@driversaddiction" target='https://www.youtube.com/@driversaddiction'><FaYoutube size={30} color="red" className='m-5 drop-shadow-[0_2px_1px_rgba(0,0,0,0.25)] hover:drop-shadow-[0_4px_1px_rgba(0,0,0,0.25)] transition duration-5s' /></Link>
--- frontend/src/pages/Trending.jsx
+++ frontend/src/pages/Trending.jsx
@@ -6,34 +6,33 @@ const Trending = () => {
   return (
     <div className='m-0 h-full w-full flex flex-col items-center  bg-gradient-to-br from-white to-stone-300'>
       <Nav/>
-      <div className="w-full h-40 md:h-500 lg:h-96 max-w-screen-lg bg-no-repeat bg-cover bg-center  bg-[url('https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/2023/Products/Concept-Car/Studio/_DSF4308_ret_lores.jpg/jcr:content/_DSF4308_ret_lores.jpg')]">
+      <div className="w-full h-40 md:h-500 lg:h-96 max-w-screen-lg bg-no-repeat bg-cover bg-center  bg-[url('https://hips.hearstapps.com/hmg-prod/images/2025-toyota-4runner-trdpro-104-jpg-6615617e361a0.jpg?crop=0.786xw:0.719xh;0.119xw,0.161xh&resize=980:*')]">
         </div>
 
         <p className=' w-full max-w-screen-lg h-full flex flex-col items-center shadow-md bg-white'>
-    <h1 className='text-xl font-light md:text-5xl drop-shadow-[0_7px_4px_rgba(0,0,0,0.25)] text-left m-10'>Porsche Mission X</h1>
+    <h1 className='text-xl font-light md:text-5xl drop-shadow-[0_7px_4px_rgba(0,0,0,0.25)] text-left m-10'>the All-New 2025 Toyota 4Runner</h1>
     <p className=' mx-10 text-left md:text-xl lg:mx-44 md:leading-10'>
     06/08/2023
-At Porsche, innovative concept cars have always laid the groundwork for the future. The sports car manufacturer is continuing this tradition with this latest concept study. Mission X is a spectacular, conceptual reinterpretation of a hypercar, with Le Mans-style doors that open upwards to the front and an ultra high-performance, efficient electric powertrain.
+    As the automotive industry forges ahead into the future, Toyota unveils its latest masterpiece: the 2025 Toyota 4Runner lineup. With a blend of innovation, versatility, and homage to its rich heritage, the new lineup introduces two exciting trims—the Trailhunter and Platinum—joining the esteemed ranks of SR5, TRD Sport, TRD Off-Road, Limited, and TRD Pro models.
+
+Revolutionizing the driving experience, the 2025 4Runner lineup boasts enhanced performance options. Most models will harness the power of the i-Force 2.4-liter turbocharged four-cylinder engine, delivering an impressive 278 horsepower. However, for those craving an extra surge, select models will feature the i-Force Max hybrid system, integrating an additional electric motor to elevate the total output to an exhilarating 326 horsepower.
 </p>
-<div className="w-full h-40 md:h-500 md:w-3/4 lg:h-96 max-w-screen-lg bg-no-repeat bg-cover bg-center my-20  bg-[url('https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_690x388/dam/pnr/2023/Products/Concept-Car/Studio/_DSF5274_ret_lores.jpg/jcr:content/_DSF5274_ret_lores.jpg')]">
+<div className="w-full h-40 md:h-500 md:w-3/4 lg:h-96 max-w-screen-lg bg-no-repeat bg-cover bg-center my-20  bg-[url('https://hips.hearstapps.com/hmg-prod/images/2025-toyota-4runner-limited-interior-102-jpg-6615602fd2903.jpg?crop=1xw:0.9166666666666666xh;center,top&resize=980:*')]">
         </div>
     <p className=' mx-10 text-left md:text-xl lg:mx-44 md:leading-10'>
-    Porsche Mission X, 2023, Porsche AG
-Porsche Mission X: a vivid dream of incredible performance
-06/08/2023
-At Porsche, innovative concept cars have always laid the groundwork for the future. The sports car manufacturer is continuing this tradition with this latest concept study. Mission X is a spectacular, conceptual reinterpretation of a hypercar, with Le Mans-style doors that open upwards to the front and an ultra high-performance, efficient electric powertrain.
+    Beyond its powertrain, the 2025 4Runner undergoes a striking transformation in aesthetics, resembling a Tacoma SUV more closely than ever before. Drawing inspiration from the recently debuted pickup, the new 4Runner shares distinctive exterior styling cues and interior features, while also paying homage to its storied past.
 
+Trailhunter Trim: Unleashing Adventure
 
-Atlanta. Celebrating 75 years of Porsche sports cars, the storied marque today revealed its newest concept car: the Mission X. As a design study, not offered for sale, with production to be decided in due time, the Mission X is a glimpse into what the sports car of the future could look like.
+For the intrepid explorers and off-road enthusiasts, the Trailhunter trim emerges as a beacon of rugged capability. Tailored for overland expeditions, this variant combines robust performance with cutting-edge technology to conquer any terrain. From rocky trails to expansive wilderness, the Trailhunter ensures a seamless blend of adventure and comfort, promising an unforgettable journey off the beaten path.
 
+Platinum Trim: Elevating Luxury
 
-Livestream today: the ‘75 Years of Porsche Sports Cars’ anniversary show
-Today, Porsche will mark its anniversary together with invited luminaries and other important figures from Porsche’s history, alongside some of the world’s media.
+At the summit of sophistication lies the Platinum trim, epitomizing luxury and refinement. Crafted with meticulous attention to detail, this pinnacle of elegance offers an opulent driving experience. From its lavish interior adorned with premium materials to its advanced tech features, the Platinum trim sets a new standard for upscale SUVs. With unparalleled comfort and sophistication, every journey becomes a first-class voyage.
 
-more
-“The Porsche Mission X is a technology beacon for the sports car of the future. It picks up the torch of iconic sports cars of decades past: like the 959, the Carrera GT and the 918 Spyder before it, the Mission X provides critical impetus for the evolutionary development of future vehicle concepts,” says Oliver Blume, Chairman of the Executive Board of Porsche AG. “Daring to dream and dream cars are two sides of the same coin for us: Porsche has only remained Porsche by constantly changing.”
+A Glimpse into the Future
 
-Michael Mauer, Head of Style Porsche, says: “The Mission X is a clear commitment to the core of the brand. The continuing, enhanced expression of our brand and product identity is an important compass for us to navigate the development of our series-production models. The concept study symbolizes a symbiosis of unmistakable motorsport DNA with a luxurious overall impression.”
+As Toyota propels into the future with the 2025 4Runner lineup, it not only redefines the boundaries of performance and luxury but also celebrates its enduring legacy. With a nod to its iconic predecessors and a vision for tomorrow's adventures, the new 4Runner embodies the spirit of exploration and innovation. Whether traversing rugged landscapes or navigating city streets, the 2025 Toyota 4Runner stands poised to elevate every journey, beckoning drivers to embark on the road less traveled.
 </p>
         </p>
         <Footer/>
