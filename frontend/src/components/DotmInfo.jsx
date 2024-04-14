82264c29b7413c3e3e6c500866504bc0e54e673b
Jesus Perea <g-sus-15@hotmail.com>
2024-04-14 01:30:28 -0700

    fixed loop request bug

--- frontend/src/components/DotmInfo.jsx
+++ frontend/src/components/DotmInfo.jsx
@@ -4,22 +4,21 @@ import axios from 'axios'
 function Dotminfo() {
     const [driverInfo, setDriverInfo] = useState("");
 
-    async function fetchAllData() {
-      try {
-        const response = await axios.get("/api/driverOfTheWeek/6613aa132c2c7636ca911560");
-        console.log(' All post have been successfully recieved:', response.data);
-       setDriverInfo(response.data)
-        return response.data;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
+    useEffect(() => {
+      async function fetchDriver() {
+          try {
+              const response = await axios.get("/api/driverOfTheWeek/6613aa132c2c7636ca911560");
+              console.log('Data successfully received:', response.data);
+              setDriverInfo(response.data);
+          } catch (error) {
+              console.error('Error fetching data:', error);
+          }
       }
-    }
-    
-    fetchAllData()
-    
+
+      fetchDriver();
+    }, []);
   return (
-    <div className=' text-center m-5'>
+    <div className=' text-center'>
     <h6>Welcome to Driver of The Month Edit</h6>
     <img src={driverInfo.heroImage} alt='banner of the driver' className=' w-40 h-20' />
     <img src={driverInfo.profileImage} alt='profile image of the driver' className='w-36 h-36'/>
